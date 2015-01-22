0645cde74d78547506e9ad9d2ffb67d906f030de
Dan Miller <x.dan.miller@outlook.com>
2015-01-22 18:54:31 -0500

    Added icon

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['pear.py'])
+setup(
+    console=[
+        {
+            'script': 'pear.py',
+            'icon_resources': [(0, "pear.ico")]
+        }
+    ]
+)
